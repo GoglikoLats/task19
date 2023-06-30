b018da9d4a473e9fe7c57b2e924bfcb1747fc829
MacBook Pro <macintoshi@MacBook-pro-MacBook-Pro.local>
2023-06-30 16:16:52 +0400

    task19

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('open-btn').addEventListener('click', function() {
+    document.getElementById('burger-toggle').checked = true;
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('burger-toggle').checked = false;
+});
